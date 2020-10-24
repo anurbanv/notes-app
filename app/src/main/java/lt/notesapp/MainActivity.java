fd9c6a9afd7c748582199c28f4e2d6c1560011c7
Urban Andrius <urban.andrius@gmail.com>
2020-10-24 16:50:22 +0300

    Added note view and model

--- app/src/main/java/lt/notesapp/MainActivity.java
+++ app/src/main/java/lt/notesapp/MainActivity.java
@@ -1,14 +1,27 @@
 package lt.notesapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import lt.notesapp.model.Note;
+import lt.notesapp.views.NoteView;
+
 public class MainActivity extends AppCompatActivity {
+
+    lt.notesapp.databinding.ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = lt.notesapp.databinding.ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Note note = new Note("Title", "Content");
+        NoteView noteView = new NoteView(this);
+
+        binding.llRoot.addView(noteView);
+        noteView.update(note);
+
     }
 }
